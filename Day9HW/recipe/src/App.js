0a2a7f2049b1971c930e57c4e74e7ddfe2d673af
Jeffrey Cheng <jeffreycheng1230@gmail.com>
2022-06-14 02:36:15 -0400

    cleaned up day 9 hw

--- Day9HW/recipe/src/App.js
+++ Day9HW/recipe/src/App.js
@@ -26,6 +26,33 @@ export default function App() {
     let item = new Item(curItem.name, curItem.quantity, curItem.description);
     item = await RecipeServices.addItem(item);
     setItems([...items, item]);
+    setCurItem(new Item("", "", ""));
+  }
+
+  async function updateItem(name, quantity, description, id) {
+    let tempArr = items.slice();
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id == id) {
+        tempArr[i].name = name;
+        tempArr[i].quantity = quantity;
+        tempArr[i].description = description;
+        break;
+      }
+    }
+    setItems(tempArr);
+    await RecipeServices.editItem(name, quantity, description, id);
+  }
+
+  async function deleteItem(id) {
+    let tempArr = items.slice();
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id == id) {
+        tempArr.splice(i, 1);
+        break;
+      }
+    }
+    setItems(tempArr);
+    await RecipeServices.removeItem(id);
   }
 
   return (
@@ -33,38 +60,43 @@ export default function App() {
       <div className="container">
         <div className="card text-center p-3">
           <h1> Ingredients List </h1>
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Name"
-              onChange={editName}
-            ></input>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Quantity"
-              onChange={editQuantity}
-            ></input>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Description"
-              onChange={editDescription}
-            ></input>
-            <button
-              className="btn btn-secondary input-group-append"
-              onClick={appendItem}
-            >
-              +
-            </button>
-          </div>
+          <form>
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={curItem.name}
+                onChange={editName}
+              ></input>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Quantity"
+                value={curItem.quantity}
+                onChange={editQuantity}
+              ></input>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Description"
+                value={curItem.description}
+                onChange={editDescription}
+              ></input>
+              <button
+                type="submit"
+                className="btn btn-secondary input-group-append"
+                onClick={appendItem}
+              >
+                +
+              </button>
+            </div>
+          </form>
           <hr></hr>
           <IngredientList
             items={items}
-            editName={editName}
-            editQuantity={editQuantity}
-            editDescription={editDescription}
+            updateItem={updateItem}
+            deleteItem={deleteItem}
           ></IngredientList>
         </div>
       </div>
--- Day9HW/recipe/src/components/IngredientList.js
+++ Day9HW/recipe/src/components/IngredientList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Item } from "../models/Item";
 
 export function IngredientList(props) {
   return (
@@ -14,32 +16,61 @@ export function IngredientList(props) {
         <tbody>
           {props.items.map((item) => (
             <tr key={item.id}>
-                <td>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Name"
-                    value={item.name}
-                    onChange={props.editName}
-                  ></input>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Quantity"
-                    value={item.quantity}
-                    onChange={props.editQuantity}
-                  ></input>
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={item.description}
-                    onChange={props.editDescription}
-                  ></input>
-                </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  value={item.name}
+                  onChange={(e) =>
+                    props.updateItem(
+                      e.target.value,
+                      item.quantity,
+                      item.description,
+                      item.id
+                    )
+                  }
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Quantity"
+                  value={item.quantity}
+                  onChange={(e) =>
+                    props.updateItem(
+                      item.name,
+                      e.target.value,
+                      item.description,
+                      item.id
+                    )
+                  }
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={item.description}
+                  onChange={(e) =>
+                    props.updateItem(
+                      item.name,
+                      item.quantity,
+                      e.target.value,
+                      item.id
+                    )
+                  }
+                ></input>
+              </td>
+              <td>
+                <button
+                  className="btn"
+                  onClick={(e) => props.deleteItem(item.id)}
+                >
+                  <i className="bi bi-trash-fill"></i>
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
